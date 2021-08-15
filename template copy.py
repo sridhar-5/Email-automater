a174efceeb304f99ad98ec2090b23cb10fa6c0e0
Venkata Sridhar Sai <kv.sridharsai@gmail.com>
2021-08-15 11:09:43 +0530

    Delete template copy.py

--- template copy.py
+++
@@ -1,8 +0,0 @@
-from string import Template
-
-def readTemplate():
-    MessageFile = open("message.txt","r")
-    MessageFileContent = MessageFile.read()
-
-    #returning the template object made from the content of the message file
-    return Template(MessageFileContent)
